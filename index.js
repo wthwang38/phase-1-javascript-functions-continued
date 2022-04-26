778adebe5a75ccb14b25cd1ca8f93e2df56f9561
William Hwang <willhwang90@gmail.com>
2022-04-26 11:21:28 -0400

    commit tree cfd873f4908e9da112d4a9049742d9e756ca73ff

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+} //function with a default
+
+const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+} //function expression with a default
+
+function wrapAdjective(flair = "*"){
+    return function(word = "special"){
+            return `You are ${flair}${word}${flair}!`;
+        }
+    }
